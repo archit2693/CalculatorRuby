3cca2d6c05beb0998f2967645bfc6084e6a0a68c
Archit joshi <architjoshi@Archits-MacBook-Pro.local>
2018-05-15 18:57:02 +0530

    Added exit and cancel functions and loop for continous inputs

--- calculator.rb
+++ calculator.rb
@@ -24,37 +24,58 @@ def divide(input_number)
   return $number
 end
 
+# To cancel the operations or clear $number
+def  cancel_operations()
+  $number = 0.0
+end
+
 class String
   def is_integer?
     self.to_i.to_s == self
   end
 end
 
-# Reading users command
-input_command_from_user = gets.chomp.to_s
-split_command_array = input_command_from_user.split(" ")
+loop do
+  # Reading users command
+  input_command_from_user = gets.chomp.to_s
+  split_command_array = input_command_from_user.split(" ")
 
-# Checking for command without any operand
-if(split_command_array.length == 2)
-  command_string = split_command_array[0]
-  operand = split_command_array[1]
-  if(operand.is_integer?)     # Checking for commands like 'add abc' where operand not an number
-    operand = operand.to_f    # Converting operand to float
+  # Checking for command without any operand
+  if(split_command_array.length == 2)
+    command_string = split_command_array[0]
+    operand = split_command_array[1]
+    if(operand.is_integer?)     # Checking for commands like 'add abc' where operand not an number
+      operand = operand.to_f    # Converting operand to float
+
+      case command_string
+      when "add"
+        puts add_numbers(operand)
+      when "subtract"
+        puts subtract(operand)
+      when "multiply"
+        puts multiply(operand)
+      when "divide"
+        puts divide(operand)
+      else
+        puts "Sorry, This is an invalid operation."
+      end
+    else
+      puts "Invalid Operand !"
+    end  
+    
+  else
+    command_string = split_command_array[0]
 
     case command_string
-    when "add"
-      puts add_numbers(operand)
-    when "subtract"
-      puts subtract(operand)
+    when "cancel"
+      puts cancel_operations()
+    when "exit"
+      puts "Good Bye !"
+      break
     else
       puts "Sorry, This is an invalid operation."
     end
-  else
-    puts "Invalid Operand !"
-  end  
-  
-else
-  puts "Invalid command ! "
+  end
 end
 
 
